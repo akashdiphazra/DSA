2431cc9342d7322107d5c8465bce3c599c0363a8
akashdiphazra <princehazra.96@gmail.com>
2022-08-31 11:05:19 +0530

    spacing changed

--- C++/Data Structures/Graphs/Unweighted_Graph.cpp
+++ C++/Data Structures/Graphs/Unweighted_Graph.cpp
@@ -5,42 +5,42 @@
 
 namespace Graph {
 class Graph {
- private:
-    int vertices;
-    std::vector<std::vector<int>> adjlists;
+private:
+  int vertices;
+  std::vector<std::vector<int>> adjlists;
 
- public:
-    Graph(int v) {
-        vertices = v;
-        adjlists.resize(v);
-    }
+public:
+  Graph(int v) {
+    vertices = v;
+    adjlists.resize(v);
+  }
 
-    void add_edge(int u, int v) {
-        this->adjlists[u].push_back(v);
-        this->adjlists[v].push_back(u); /* For undirected graph */
-    }
+  void add_edge(int u, int v) {
+    this->adjlists[u].push_back(v);
+    this->adjlists[v].push_back(u); /* For undirected graph */
+  }
 
-    void print_graph() {
-        for (int i = 0; i < vertices; i++) {
-            std::cout << i << " ➡ ";
-            for (auto v : adjlists[i]) {
-                std::cout << v << " ";
-            }
-            std::cout << std::endl;
-        }
+  void print_graph() {
+    for (int i = 0; i < vertices; i++) {
+      std::cout << i << " ➡ ";
+      for (auto v : adjlists[i]) {
+        std::cout << v << " ";
+      }
+      std::cout << std::endl;
     }
+  }
 };
-};  // namespace Graph
+}; // namespace Graph
 
 int main() {
-    Graph::Graph g(6);
-    g.add_edge(0, 1);
-    g.add_edge(1, 5);
-    g.add_edge(5, 4);
-    g.add_edge(4, 0);
-    g.add_edge(4, 3);
-    g.add_edge(3, 2);
-    g.add_edge(0, 2);
-    g.print_graph();
-    return 0;
+  Graph::Graph g(6);
+  g.add_edge(0, 1);
+  g.add_edge(1, 5);
+  g.add_edge(5, 4);
+  g.add_edge(4, 0);
+  g.add_edge(4, 3);
+  g.add_edge(3, 2);
+  g.add_edge(0, 2);
+  g.print_graph();
+  return 0;
 }
