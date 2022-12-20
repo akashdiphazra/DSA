87798a911e68760accedbed38ef4bd69668abc85
akashdiphazra <princehazra.96@gmail.com>
2022-12-20 20:30:02 +0530

    Armstrong_Number number added.

--- C++/Algorithms/Dynamic Programming/Armstrong_Number.cpp
+++ C++/Algorithms/Dynamic Programming/Armstrong_Number.cpp
@@ -1,6 +1,36 @@
+#include <cmath>
 #include <iostream>
 
+namespace Dynamic_Programming {
+class Armstrong_Number {
+public:
+  bool find(int_fast64_t num) {
+    int temp{0}, rem{0}, count{0}, sum{0};
+    temp = num;
+    while (temp != 0) {
+      temp /= 10;
+      count++;
+    }
+    temp = num;
+    while (temp != 0) {
+      rem = temp % 10;
+      sum += static_cast<int>(std::pow(rem, count));
+      temp /= 10;
+    }
+    if (sum == num) {
+      return true;
+    } else {
+      return false;
+    }
+    return false;
+  }
+};
+}; // namespace Dynamic_Programming
+
 int main() {
-    std::cout << "Hello World" << std::endl;
-    return 0;
+  Dynamic_Programming::Armstrong_Number an;
+  std::cout << std::boolalpha << an.find(371) << std::endl;
+  std::cout << std::boolalpha << an.find(153) << std::endl;
+  std::cout << std::boolalpha << an.find(377) << std::endl;
+  return 0;
 }
